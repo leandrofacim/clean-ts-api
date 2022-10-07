b303f475d2d8312917271f9c2753aa5cdb985d63
Leandro Facim <leandro.facim@hotmail.com>
2022-10-06 22:00:45 -0300

    feat: add SignUp Factory

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { BcryptAdapter } from './bcrypt-adapter'
+import { BcryptAdapter } from './cryptography'
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
