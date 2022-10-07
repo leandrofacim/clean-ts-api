e12b8b0542fb32f7a3394567f5f75fea1a91ac6f
Leandro Facim <leandro.facim@hotmail.com>
2022-10-07 14:13:30 -0300

    chore: change jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**/'],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
